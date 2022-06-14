58d96f3d5ec7515fe18132f5d1cde480cd7cbe7d
Dominique Kornegay <dominiquekornegay@gmail.com>
2022-06-14 16:11:36 -0400

    commit tree 462330b2a4b6730064a09547f442c16f4ead8143

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+   let item = document.getElementById("text");
+   
+    item.textContent = "This is really cool!"
+    item.append})
